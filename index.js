8c510c113880436e99427887182ce78245b2d447
kwamebrowne <theeecryptoking@gmail.com>
2023-04-12 17:12:45 -0500

    Finished

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function addingEventListener(){
     input.addEventListener('click', function() {
     alert('I was clicked!');
     });
-}
+}//Recheck for box to pop up in html
 /*Does not Work
 const input = document.getElementById('button');
 function addingEventListener(){
@@ -11,4 +11,5 @@ function addingEventListener(){
         alert('I was clicked!');
       }
 }
-input.addEventListener('click', clickAlert);*/
+input.addEventListener('click', clickAlert);*/ 
+//Box popped with this function 
